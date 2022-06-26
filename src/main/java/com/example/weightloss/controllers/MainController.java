52fcc28f2f395e9bd228bab267a9d3270ef6f22a
vitaly gavrilchenko <vitgavrilchenko@gmail.com>
2022-06-26 14:45:53 +0300

    weight list instead of weight

--- src/main/java/com/example/weightloss/controllers/MainController.java
+++ src/main/java/com/example/weightloss/controllers/MainController.java
@@ -4,7 +4,9 @@ import com.example.weightloss.entities.Weight;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class MainController {
@@ -14,12 +16,16 @@ public class MainController {
         return "hello. this is  the test message";
     }
 
-    @RequestMapping("/weight")
-    public Weight getWeight() {
-        Weight weight = new Weight();
-        weight.setWeight("75.5");
-        weight.setDateTime(LocalDateTime.now());
-        return weight;
+    @RequestMapping("/weight-list")
+    public List<Weight> getWeightList() {
+        Weight weight1 = new Weight();
+        weight1.setWeight("75.5");
+        weight1.setDateTime(LocalDate.of(2022, 6, 26));
+        Weight weight2 = new Weight();
+        weight2.setWeight("73.4");
+        weight2.setDateTime(LocalDate.of(2022, 6, 25));
+
+        return List.of(weight1, weight2, new Weight());
     }
 
 }
--- src/main/java/com/example/weightloss/entities/Weight.java
+++ src/main/java/com/example/weightloss/entities/Weight.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -15,5 +15,5 @@ public class Weight {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String weight;
-    private LocalDateTime dateTime;
+    private LocalDate dateTime;
 }
